d9fda51c7720acdd223a8d81443dec87f4a7bf25
John Spurlock <john@skymethod.com>
2022-11-10 18:58:30 -0600

    Add all patterns files to test, fix any failures found.

--- .github/workflows/patterns_test.ts
+++ .github/workflows/patterns_test.ts
@@ -4,39 +4,87 @@ import { join, fromFileUrl } from 'https://deno.land/std@0.163.0/path/mod.ts';
 Deno.test({
     name: 'patterns',
     fn: async () => {
-        const filepath = join(fromFileUrl(import.meta.url), '../../../patterns/apps.json');
-        const txt = await Deno.readTextFile(filepath);
-        const obj = JSON.parse(txt);
-        if (!Array.isArray(obj.entries)) fail(`Bad top-level object: missing 'entries' array.`);
-        const names = new Set<string>();
-        obj.entries.forEach((entry: unknown, i: number) => {
-            if (typeof entry !== 'object' || entry === null) fail(`Bad entry[${i}]: expected an object, found ${JSON.stringify(entry)}`);
-            const { name, pattern, description, examples } = entry as Record<string, unknown>;
+        for (const file of [ 'apps', 'bots', 'browsers', 'devices', 'libraries', 'referrers' ]) {
+            const filepath = join(fromFileUrl(import.meta.url), `../../../patterns/${file}.json`);
+            const txt = await Deno.readTextFile(filepath);
+            const obj = JSON.parse(txt);
+            if (!Array.isArray(obj.entries)) fail(`Bad top-level object: missing 'entries' array.`);
+            const names = new Set<string>();
+            let i = 0;
+            for (const entry of obj.entries) {
+                const tag = `${file}.entry[${i}]`;
+                if (typeof entry !== 'object' || entry === null) fail(`Bad ${tag}: expected an object, found ${JSON.stringify(entry)}`);
 
-            if (typeof name !== 'string') fail(`Bad entry[${i}].name: expected a string property, found ${JSON.stringify(entry)}`);
-            if (name.trim() !== name) fail(`Bad entry[${i}].name: expected no leading or trailing whitespace, found ${name}`);
-            if (name === '') fail(`Bad entry[${i}].name: expected a non-blank string`);
-            if (names.has(name.toLowerCase())) fail(`Bad entry[${i}].name: expected a unique value, found ${name}`);
-            names.add(name.toLowerCase());
+                const { name, pattern, description, examples, svg, comments, type, urls } = entry as Record<string, unknown>;
 
-            if (typeof pattern !== 'string') fail(`Bad entry[${i}].pattern: expected a string property, found ${JSON.stringify(entry)}`);
-            if (/^\s+$/.test(pattern)) fail(`Bad entry[${i}].pattern: expected a non-blank string`);
+                // name
+                if (typeof name !== 'string') fail(`Bad ${tag}.name: expected a string property, found ${JSON.stringify(entry)}`);
+                if (name.trim() !== name) fail(`Bad ${tag}.name: expected no leading or trailing whitespace, found ${name}`);
+                if (name === '') fail(`Bad ${tag}.name: expected a non-blank string`);
+                if (names.has(name.toLowerCase())) fail(`Bad ${tag}.name: expected a unique value, found ${name}`);
+                names.add(name.toLowerCase());
 
-            if (description !== undefined && typeof description !== 'string') fail(`Bad entry[${i}].description: expected an optional string property, found ${JSON.stringify(entry)}`);
-            if (typeof description === 'string') {
-                if (description.trim() !== description) fail(`Bad entry[${i}].description: expected no leading or trailing whitespace, found ${description}`);
-                if (description === '') fail(`Bad entry[${i}].description: expected a non-blank string`);
+                // pattern
+                if (typeof pattern !== 'string') fail(`Bad ${tag}.pattern: expected a string property, found ${JSON.stringify(entry)}`);
+                if (/^\s+$/.test(pattern)) fail(`Bad ${tag}.pattern: expected a non-blank string`);
+                const regex = new RegExp(pattern);
+
+                // description
+                if (description !== undefined && typeof description !== 'string') fail(`Bad ${tag}.description: expected an optional string property, found ${JSON.stringify(entry)}`);
+                if (typeof description === 'string') {
+                    if (description.trim() !== description) fail(`Bad ${tag}.description: expected no leading or trailing whitespace, found ${description}`);
+                    if (description === '') fail(`Bad ${tag}.description: expected a non-blank string`);
+                }
+
+                // svg
+                if (svg !== undefined && typeof svg !== 'string') fail(`Bad ${tag}.svg: expected an optional string property, found ${JSON.stringify(entry)}`);
+                if (typeof svg === 'string') {
+                    if (!/^[a-z]+(-[a-z]+)*\.svg$/.test(svg)) fail(`Bad ${tag}.svg: unexpected value ${JSON.stringify(svg)}`);
+                    await Deno.stat(join(fromFileUrl(import.meta.url), `../../../svg/${svg}`));
+                }
+
+                // comments
+                if (comments !== undefined && typeof comments !== 'string') fail(`Bad ${tag}.comments: expected an optional string property, found ${JSON.stringify(entry)}`);
+                if (typeof comments === 'string') {
+                    if (comments.trim() !== comments) fail(`Bad ${tag}.comments: expected no leading or trailing whitespace, found ${comments}`);
+                    if (comments === '') fail(`Bad ${tag}.comments: expected a non-blank string`);
+                }
+
+                // examples
+                if (examples !== undefined) {
+                    if (!Array.isArray(examples)) fail(`Bad ${tag}.examples: expected an array, found ${JSON.stringify(examples)}`);
+                    examples.forEach((example: unknown, j: number) => {
+                        if (typeof example !== 'string') fail(`Bad ${tag}.examples[${j}]: expected a string, found ${JSON.stringify(example)}`);
+                        if (!regex.test(example)) fail(`Bad ${tag}.examples[${j}]: \"${example}\" does not match pattern \"${pattern}\"`);
+                    });
+                }
+
+                // urls
+                if (urls !== undefined) {
+                    if (!Array.isArray(urls)) fail(`Bad ${tag}.urls: expected an array, found ${JSON.stringify(urls)}`);
+                    urls.forEach((url: unknown, j: number) => {
+                        if (typeof url !== 'string') fail(`Bad ${tag}.urls[${j}]: expected a string, found ${JSON.stringify(url)}`);
+                        if (!isValidUrl(url)) fail(`Bad ${tag}.urls[${j}]: expected url, found \"${url}\"`);
+                    });
+                }
+
+                // type
+                if (type !== undefined && typeof type !== 'string') fail(`Bad ${tag}.type: expected an optional type property, found ${JSON.stringify(entry)}`);
+                if (typeof type === 'string') {
+                    if (!/^[a-z]+(_[a-z]+)*$/.test(type)) fail(`Bad ${tag}.type: unexpected value ${JSON.stringify(type)}`);
+                }
+
+                i++;
             }
-
-            const regex = new RegExp(pattern);
-            if (examples !== undefined) {
-                if (!Array.isArray(examples)) fail(`Bad entry[${i}].examples: expected an array, found ${JSON.stringify(examples)}`);
-                examples.forEach((example: unknown, j: number) => {
-                    if (typeof example !== 'string') fail(`Bad entry[${i}].examples[${j}]: expected a string, found ${JSON.stringify(example)}`);
-                    if (!regex.test(example)) fail(`Bad entry[${i}].examples[${j}]: \"${example}\" does not match pattern \"${pattern}\"`);
-                });
-            }
-
-        });
+        }
     }
 });
+
+function isValidUrl(url: string): boolean {
+    try {
+        const u = new URL(url);
+        return u.protocol === 'http:' || u.protocol === 'https:'
+    } catch {
+        return false;
+    }
+}
