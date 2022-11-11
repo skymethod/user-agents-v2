bb8c1e6ee2670e19d70210b0a4a8ead61871830a
John Spurlock <john@skymethod.com>
2022-11-10 18:10:40 -0600

    Start setting up github action on every push and pull request.

---
+++ .github/patterns_test.ts
@@ -0,0 +1,18 @@
+import { fail } from 'https://deno.land/std@0.163.0/testing/asserts.ts';
+import { join, fromFileUrl } from 'https://deno.land/std@0.163.0/path/mod.ts';
+
+Deno.test({
+    name: 'patterns',
+    fn: async () => {
+        const filepath = join(fromFileUrl(import.meta.url), '../../patterns/apps.json');
+        const txt = await Deno.readTextFile(filepath);
+        const obj = JSON.parse(txt);
+        if (!Array.isArray(obj.entries)) fail(`Bad top-level object: missing 'entries' array.`);
+        obj.entries.forEach((entry: unknown, i: number) => {
+            if (typeof entry !== 'object' || entry === null) fail(`Bad entry[${i}]: expected an object, found ${JSON.stringify(entry)}`);
+            const { name, pattern } = entry as Record<string, unknown>;
+            if (typeof name !== 'string') fail(`Bad entry[${i}]: expected a 'name' property, found ${JSON.stringify(entry)}`);
+            if (typeof pattern !== 'string') fail(`Bad entry[${i}]: expected a 'pattern' property, found ${JSON.stringify(entry)}`);
+        });
+    }
+});
